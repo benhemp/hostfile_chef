65af1033c1a89134838c002ea7b6ece80108fdec
Todd Downey <toddd79@gmail.com>
2018-08-26 15:31:04 -0500

    Adding Travis-CI config
    Adding config for the dev cluster.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,8 @@
-# !/bin/bash
+#!/usr/bin/env bash
 
 # We don't need to install our gems as it comes from chefdk above
 echo "skip bundle install"
+
+# Installing needed packages
+/opt/chefdk/embedded/bin/chef gem install rubocop
+/opt/chefdk/embedded/bin/opt/chefdk/embedded/bin/chef gem install kitchen-docker
